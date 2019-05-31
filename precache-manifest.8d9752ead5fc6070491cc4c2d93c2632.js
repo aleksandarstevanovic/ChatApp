d3885dd92ec25f11b167bb183b7902fac3712a01
Aleksandar Stevanovic <acastev@gmail.com>
2019-05-31 03:25:30 -0700

    Updates

---
+++ precache-manifest.8d9752ead5fc6070491cc4c2d93c2632.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433723d351bde3054c7f72fcbe2cfb53",
+    "url": "/aleksandarstevanovic/chatapp/index.html"
+  },
+  {
+    "revision": "a79d014989f66b534ecc",
+    "url": "/aleksandarstevanovic/chatapp/static/css/main.208d9942.chunk.css"
+  },
+  {
+    "revision": "b378b283213f0499a2e0",
+    "url": "/aleksandarstevanovic/chatapp/static/js/2.26018eed.chunk.js"
+  },
+  {
+    "revision": "a79d014989f66b534ecc",
+    "url": "/aleksandarstevanovic/chatapp/static/js/main.0713c477.chunk.js"
+  },
+  {
+    "revision": "eecb00ef7db96ea106b7",
+    "url": "/aleksandarstevanovic/chatapp/static/js/runtime~main.7f4a5ad2.js"
+  }
+]);
